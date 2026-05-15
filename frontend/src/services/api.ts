7af9bb4a44563accf1f4e86cf44e1b400725ff76
lebedinvladislav <lebedinvladislav@gmail.com>
2026-05-16 00:04:01 +0700

    пробую обращаться к апи на локалхост:8080

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: 'https://localhost:8081' });
+const api = axios.create({ baseURL: 'https://localhost:8080' });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("access_token");
