99fb70f256ef255b372cd3c0e705630590fd374e
lebedinvladislav <lebedinvladislav@gmail.com>
2026-05-15 23:38:06 +0700

    добавил настройку CORS на бэк

--- backend/Program.cs
+++ backend/Program.cs
@@ -68,6 +68,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy.WithOrigins("http://127.0.0.1:5173")
+            .AllowAnyHeader()
             .AllowCredentials();
     });
 });
