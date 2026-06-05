586d0e6a66f1c63b26700483bdb21aaa8e92fd6b
lebedinvladislav <lebedinvladislav@gmail.com>
2026-06-06 00:06:10 +0700

    добавил "soursemap: true"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        sourcemap: true,
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
